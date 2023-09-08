166796b429ca81c8674f9e41ed338c444ef1000f
Conor Ferring <conorferring@gmail.com>
2023-09-08 17:18:32 -0400

    Prototype HTML & CSS set up, core Javascript functions added, pseudocode being developed.

--- myscript.js
+++ myscript.js
@@ -1,8 +1,39 @@
-const add = function(a, b) {
+let inputA = 0;
+let inputB = 0;
+
+// Get buttons from page
+const addBtn = document.getElementById('addBtn')
+const subtractBtn = document.getElementById('subtractBtn')
+const multiplyBtn = document.getElementById('multiplyBtn')
+const divideBtn = document.getElementById('divideBtn')
+const sumBtn = document.getElementById('sumBtn')
+
+// Wait for numpad buttons to be pressed, assign full number to screenValue, display on screen
+function readInputs() {
+  // inputA = ;
+  // inputB = ;
+}
+
+// Function for if + pressed, do handleOperatorPress(plus)
+addBtn.onclick = () => handleOperatorPress('add')
+subtractBtn.onclick = () => handleOperatorPress('subtract')
+multiplyBtn.onclick = () => handleOperatorPress('multiply')
+divideBtn.onclick = () => handleOperatorPress('divide')
+sumBtn.onclick = () => handleOperatorPress('sum', inputA, inputB)
+
+// function to handle num button presses, using current screen value
+function handleOperatorPress(operator, inputA, inputB) {
+  // If any OPERATOR button pressed, save current screenValue as variable A. When next num is pressed, wipe screen and display new number. 
+  // When = is pressed, save screenValue as variable B, use add function with value A and B, replace screenvalue
+
+  // Function to send screenValue to the screen
+}
+
+function add(a, b) {
 	return (a + b);
 };
 
-const subtract = function(a, b) {
+function subtract(a, b) {
 	return (a - b);
 };
 
@@ -10,7 +41,7 @@ const sum = function(array) {
   return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function(array) {
+function multiply(array) {
 	return array.reduce((total, current) => total * current);
 };
 
@@ -34,6 +65,10 @@ else {
     return fact;
 }
 };
+
+
+// Run
+readInputs();
 
 // Do not edit below this line
 // module.exports = {
